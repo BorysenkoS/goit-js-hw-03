5168a5b446d355fcedf29dc8bf33c3c31163b908
Serhii Borysenko <borisenko37@gmail.com>
2024-04-17 22:27:03 +0200

    hw (task-1, task-2)

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,15 @@
-function makeArray(firstArray, secondArray, maxLength) {}
+function makeArray(firstArray, secondArray, maxLength) {
+  let arr = firstArray.concat(secondArray);
+  let sliceArr = arr.slice(0, maxLength);
+
+  for (let i = 0; i < firstArray.length; i++) {
+    console.log(firstArray[i]);
+    if (arr.length > maxLength) {
+      return sliceArr;
+    }
+    return arr;
+  }
+}
 
 console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
