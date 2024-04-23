e09d01a784fb64a726595dd9d12cd4f07bbe6158
Serhii Borysenko <borisenko37@gmail.com>
2024-04-23 23:32:52 +0200

    made some test exercises

--- js/task-tests.js
+++ js/task-tests.js
@@ -87,3 +87,127 @@
 // 10. Напиши цикл, який виведе на консоль всі прості числа від 1 до 50.
 
 // 11. Напиши цикл, який знайде найбільший спільний дільник (НСД) двох чисел, введених користувачем.
+
+// ============================================================================================================
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// const keys = Object.keys(apartment);
+
+// for (let key of keys) {
+//   values.push(apartment[key]);
+//   console.log(values);
+// }
+
+// ================================================================================================================
+// let propCount = 0;
+
+// function countProps(object) {
+//   const keys = Object.keys(object);
+//   for (let key of keys) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     }
+//   }
+//   return propCount;
+// }
+// console.log(countProps({ name: 'Mango', age: 2 }));
+
+// ===================================================================================================================
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   const valuesSalary = Object.values(salaries);
+//   for (let key of valuesSalary) {
+//     totalSalary += key;
+//   }
+//   return totalSalary;
+// }
+
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+
+// ======================================================================================================================
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (let color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+//   console.log(hexColors);
+//   console.log(rgbColors);
+// }
+
+// =============================================================================================================
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+//   return null;
+// }
+// console.log(getProductPrice('Radar'));
+// console.log(getProductPrice('Scanner'));
+// console.log(getProductPrice('Droid'));
+// console.log(getProductPrice('Grip'));
+// console.log(getProductPrice('Engine'));
+
+// =======================================================================================================
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+
+//   const arr = [];
+//   for (let product of products) {
+//     if (product[propName]) {
+//       arr.push(product[propName]);
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log(getAllPropValues('price'));
+
+// ===============================================================================================================
+
+// function calculateTotalPrice(productName) {
+//   const products = [
+//     { name: 'Radar', price: 1300, quantity: 4 },
+//     { name: 'Scanner', price: 2700, quantity: 3 },
+//     { name: 'Droid', price: 400, quantity: 7 },
+//     { name: 'Grip', price: 1200, quantity: 9 },
+//   ];
+//   let total = 0;
+
+//   for (let product of products) {
+//     if (product.name === productName) {
+//       total = product.price * product.quantity;
+//       return total;
+//     }
+//   }
+//   return `Product ${productName} not found!`;
+// }
+
+// console.log(calculateTotalPrice('Droidss'));
